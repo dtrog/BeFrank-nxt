8737a3af6ffdb766457c3b812c85c9724fb13029
damien <dtrog@me.com>
2020-02-05 01:01:37 +0100

    Decreased waiting time for effective balance

--- src/java/nxt/Constants.java
+++ src/java/nxt/Constants.java
@@ -42,9 +42,9 @@ public final class Constants {
     public static final int MIN_BLOCKTIME_LIMIT = BLOCK_TIME - 7;
     public static final int MAX_BLOCKTIME_LIMIT = BLOCK_TIME + 7;
     public static final int BASE_TARGET_GAMMA = 64;
-    public static final int MAX_ROLLBACK = Math.max(Nxt.getIntProperty("nxt.maxRollback"), 720);
-    public static final int GUARANTEED_BALANCE_CONFIRMATIONS = isTestnet ? Nxt.getIntProperty("nxt.testnetGuaranteedBalanceConfirmations", 1440) : 1440;
-    public static final int LEASING_DELAY = isTestnet ? Nxt.getIntProperty("nxt.testnetLeasingDelay", 1440) : 1440;
+    public static final int MAX_ROLLBACK = Math.max(Nxt.getIntProperty("nxt.maxRollback"), 50);
+    public static final int GUARANTEED_BALANCE_CONFIRMATIONS = isTestnet ? Nxt.getIntProperty("nxt.testnetGuaranteedBalanceConfirmations", 100) : 100;
+    public static final int LEASING_DELAY = isTestnet ? Nxt.getIntProperty("nxt.testnetLeasingDelay", 100) : 100;
     public static final long MIN_FORGING_BALANCE_NQT = 1000 * ONE_NXT;
 
     public static final int MAX_TIMEDRIFT = 15; // allow up to 15 s clock difference
@@ -55,19 +55,19 @@ public final class Constants {
     public static final byte MAX_PHASING_VOTE_TRANSACTIONS = 10;
     public static final byte MAX_PHASING_WHITELIST_SIZE = 10;
     public static final byte MAX_PHASING_LINKED_TRANSACTIONS = 10;
-    public static final int MAX_PHASING_DURATION = 14 * 1440;
+    public static final int MAX_PHASING_DURATION = 14 * 100;
     public static final int MAX_PHASING_REVEALED_SECRET_LENGTH = 100;
 
     public static final int MAX_ALIAS_URI_LENGTH = 1000;
     public static final int MAX_ALIAS_LENGTH = 100;
 
-    public static final int MAX_ARBITRARY_MESSAGE_LENGTH = 160;
-    public static final int MAX_ENCRYPTED_MESSAGE_LENGTH = 160 + 16;
+    public static final int MAX_ARBITRARY_MESSAGE_LENGTH = 200;
+    public static final int MAX_ENCRYPTED_MESSAGE_LENGTH = 200 + 16;
 
     public static final int MAX_PRUNABLE_MESSAGE_LENGTH = 42 * 1024;
     public static final int MAX_PRUNABLE_ENCRYPTED_MESSAGE_LENGTH = 42 * 1024;
 
-    public static final int MIN_PRUNABLE_LIFETIME = isTestnet ? 1440 * 60 : 14 * 1440 * 60;
+    public static final int MIN_PRUNABLE_LIFETIME = isTestnet ? 100 * 60 : 14 * 100 * 60;
     public static final int MAX_PRUNABLE_LIFETIME;
     public static final boolean ENABLE_PRUNING;
     static {
@@ -83,13 +83,13 @@ public final class Constants {
     public static final int MAX_ACCOUNT_PROPERTY_NAME_LENGTH = 32;
     public static final int MAX_ACCOUNT_PROPERTY_VALUE_LENGTH = 160;
 
-    public static final long MAX_ASSET_QUANTITY_QNT = 1000000000L * 100000000L;
+    public static final long MAX_ASSET_QUANTITY_QNT = 1000000000L * 1000000000L;
     public static final int MIN_ASSET_NAME_LENGTH = 3;
     public static final int MAX_ASSET_NAME_LENGTH = 10;
     public static final int MAX_ASSET_DESCRIPTION_LENGTH = 1000;
     public static final int MAX_SINGLETON_ASSET_DESCRIPTION_LENGTH = 160;
     public static final int MAX_ASSET_TRANSFER_COMMENT_LENGTH = 1000;
-    public static final int MAX_DIVIDEND_PAYMENT_ROLLBACK = 1441;
+    public static final int MAX_DIVIDEND_PAYMENT_ROLLBACK = 1001;
 
     public static final int MAX_POLL_NAME_LENGTH = 100;
     public static final int MAX_POLL_DESCRIPTION_LENGTH = 1000;
@@ -112,11 +112,11 @@ public final class Constants {
     public static final int MIN_CURRENCY_CODE_LENGTH = 3;
     public static final int MAX_CURRENCY_CODE_LENGTH = 6;
     public static final int MAX_CURRENCY_DESCRIPTION_LENGTH = 1000;
-    public static final long MAX_CURRENCY_TOTAL_SUPPLY = 1000000000L * 100000000L;
+    public static final long MAX_CURRENCY_TOTAL_SUPPLY = 1000000000L * 1000000000L;
     public static final int MAX_MINTING_RATIO = 10000; // per mint units not more than 0.01% of total supply
     public static final byte MIN_NUMBER_OF_SHUFFLING_PARTICIPANTS = 3;
     public static final byte MAX_NUMBER_OF_SHUFFLING_PARTICIPANTS = 30; // max possible at current block payload limit is 51
-    public static final short MAX_SHUFFLING_REGISTRATION_PERIOD = (short)1440 * 7;
+    public static final short MAX_SHUFFLING_REGISTRATION_PERIOD = (short)100 * 7;
     public static final short SHUFFLING_PROCESSING_DEADLINE = (short)(isTestnet ? 10 : 100);
 
     public static final int MAX_TAGGED_DATA_NAME_LENGTH = 100;
@@ -127,7 +127,7 @@ public final class Constants {
     public static final int MAX_TAGGED_DATA_FILENAME_LENGTH = 100;
     public static final int MAX_TAGGED_DATA_DATA_LENGTH = 42 * 1024;
 
-    public static final int MAX_REFERENCED_TRANSACTION_TIMESPAN = 60 * 1440 * 60;
+    public static final int MAX_REFERENCED_TRANSACTION_TIMESPAN = 60 * 100 * 60;
     public static final int CHECKSUM_BLOCK_1 = Integer.MAX_VALUE;
 
     public static final int LAST_CHECKSUM_BLOCK = 0;
